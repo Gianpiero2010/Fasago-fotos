bb36001faafde00a98cd227bf1f7320d9b04fdb9
Gianpiero2010 <pierosanchezgomez2010@gmail.com>
2025-04-03 18:45:09 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,9 @@
-// Configuración de Firebase (reemplaza con tus datos)
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+// Tu configuración (NO compartas esto públicamente en producción)
 const firebaseConfig = {
   apiKey: "AIzaSyCkNgikdUByZmfAIAMcvBiw5t9hKewtFPQ",
   authDomain: "fasago-fotos-4a5fd.firebaseapp.com",
@@ -8,5 +13,10 @@ const firebaseConfig = {
   appId: "1:271898420621:web:6010ea98a75b2ec257c9c4"
 };
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
+// Inicialización
+const app = initializeApp(firebaseConfig);
+
+// Servicios
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);

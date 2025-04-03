6ed3a0237e89fea8abdaaa19240176fef84cc50c
Gianpiero2010 <pierosanchezgomez2010@gmail.com>
2025-04-03 18:35:03 -0500

    Delete logout.js

--- logout.js
+++
@@ -1,8 +0,0 @@
-// Función para cerrar sesión
-function logout() {
-    // Eliminar la clave "authenticated" del localStorage
-    localStorage.removeItem("authenticated");
-
-    // Redirigir al login
-    window.location.href = "login.html";
-}
